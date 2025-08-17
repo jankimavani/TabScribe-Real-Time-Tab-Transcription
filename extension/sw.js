9e9023527228f6b24fc630c937b7134e10ecc9ca
jankimavani <jankimavani2020@gmail.com>
2025-08-16 22:04:35 -0500

    update-sw

--- extension/sw.js
+++ extension/sw.js
@@ -1,2 +1,10 @@
 self.addEventListener("install", () => self.skipWaiting());
 self.addEventListener("activate", () => self.clients.claim());
+
+// Ensure our panel file is set, then open the panel when the toolbar icon is clicked
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.sidePanel.setOptions({ path: "sidepanel.html" }).catch(console.error);
+  chrome.sidePanel
+    .setPanelBehavior({ openPanelOnActionClick: true })
+    .catch(console.error);
+});
